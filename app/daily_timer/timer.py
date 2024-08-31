037ef0bad14d618444bf093981efb4f0a918b169
wiered <wieredsh@gmail.com>
2024-08-31 17:30:56 +0300

    added timer to daily updates of db

---
+++ app/daily_timer/timer.py
@@ -0,0 +1,49 @@
+import asyncio
+from datetime import datetime
+import logging
+
+from db import getAllGroupsList, saveMonthLessonsToDB
+from ruzparser import RuzParser
+
+class Timer:
+    def __init__(self, timeout, callback):
+        self._timeout = timeout
+        self._callback = callback
+        self._task = asyncio.ensure_future(self._job())
+
+    async def _job(self):
+        await asyncio.sleep(self._timeout)
+        await self._callback()
+
+    def cancel(self):
+        self._task.cancel()
+
+async def isParsingTime():
+    hour = int(datetime.today().strftime('%H'))
+    if hour == 12 or hour == 17:
+        await parseMonthlyScheduleForGroups()
+
+async def parseMonthlyScheduleForGroups():
+    await asyncio.sleep(0.1)
+            
+    print(getAllGroupsList())
+    parser = RuzParser()
+    for group in getAllGroupsList():
+        lessons_for_group = await parser.parseThisMonth(group)
+        print(type(lessons_for_group))
+        saveMonthLessonsToDB(group, lessons_for_group)
+            
+    return
+
+async def timerPooling():
+    polling = True
+    try:
+        while polling:
+            timer = Timer(10, isParsingTime)
+            await asyncio.sleep(3600)
+    except KeyboardInterrupt:
+        return
+    finally:
+        polling = False
+        return
+    
