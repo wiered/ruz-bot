de7f59831fcc59342f89abfcefc064a19600bf7d
wiered <wieredsh@gmail.com>
2024-09-04 03:17:48 +0300

    Some refactoring in db/db.py

--- app/db/db.py
+++ app/db/db.py
@@ -60,6 +60,30 @@ class DataBase():
         # If the group is not found, it is not cached
         return False
     
+    def isDateRangeInDB(self, group_id: str, start: datetime, end: datetime) -> bool:
+        # If the group is not cached, the day is not cached
+        # If group is not chached, the day is not cached
+        if not self.isGroupInDB(group_id):
+            return False
+        
+        # Get the bounds of the previous and next month
+        reference_date = datetime.now()
+        start_of_previous_month, end_of_next_month = utils.getPreviousAndNextMonthBounds(reference_date)
+        
+        # Check if the start of range is before the start of the previous month
+        if (start - start_of_previous_month).total_seconds() < 0:
+            # If it is, the range is not cached
+            return False
+        
+        # Check if the end of range is after the end of the next month
+        if (end_of_next_month - end).total_seconds() < 0:
+            # If it is, the range is not cached
+            return False
+        
+        # If the date is between the start of the previous month and the end of the next month
+        # and the group is cached, then the day is cached
+        return True
+    
     def isDayInDB(self, group_id, date: datetime) -> bool:
         """
         Check if the day is cached in the database
@@ -71,29 +95,8 @@ class DataBase():
         Returns:
             bool: True if the day is cached in the database, otherwise False
         """
-        # If the group is not cached, the day is not cached
-        # If group is not chached, the day is not cached
-        if not self.isGroupInDB(group_id):
-            return False
+        return self.isDateRangeInDB(group_id, date, date)
         
-        # Get the bounds of the previous and next month
-        reference_date = datetime.now()
-        start_of_previous_month, end_of_next_month = utils.getPreviousAndNextMonthBounds(reference_date)
-        
-        # Check if the date is before the start of the previous month
-        if (date - start_of_previous_month).total_seconds() < 0:
-            # If it is, the day is not cached
-            return False
-        
-        # Check if the date is after the end of the next month
-        if (end_of_next_month - date).total_seconds() < 0:
-            # If it is, the day is not cached
-            return False
-        
-        # If the date is between the start of the previous month and the end of the next month
-        # and the group is cached, then the day is cached
-        return True
-
     def isWeekInDB(self, group_id, date):
         """
         Check if the week is cached in the database
@@ -106,26 +109,7 @@ class DataBase():
             bool: True if the week is cached in the database, otherwise False
         """
         start, end = utils.getStartAndEndOfWeek(date)
-        if not self.isGroupInDB(group_id):
-            # If the group is not cached, the week is not cached
-            return False
-        
-        reference_date = datetime.now()
-        start_of_previous_month, end_of_next_month = utils.getPreviousAndNextMonthBounds(reference_date)
-        
-        # Check if the week is before the start of the previous month
-        if (start - start_of_previous_month).total_seconds() < 0:
-            # If it is, the week is not cached
-            return False
-        
-        # Check if the week is after the end of the next month
-        if (end_of_next_month - end).total_seconds() < 0:
-            # If it is, the week is not cached
-            return False
-        
-        # If the week is between the start of the previous month and the end of the next month
-        # and the group is cached, then the week is cached
-        return True
+        return self.isDateRangeInDB(group_id, start, end)
     
     def getUser(self, user_id):
         return self.users.find_one({"id": user_id})
@@ -161,6 +145,23 @@ class DataBase():
                               }
                               )
     
+    def getLessonsInDateRange(self, all_lessons_list, start_date, end_date, sub_group):
+        lessons_list = []
+        
+        for lesson in all_lessons_list:
+            # Check if the lesson is before the start of the day
+            if (start_date - lesson.get("date")).total_seconds() > 0:
+                pass
+            # Check if the lesson is after the end of the day
+            elif (lesson.get("date") - end_date).total_seconds() > 86300:
+                break
+            # If the lesson is on the given day, add it to the list
+            else:
+                if utils.isSubGroupValid(lesson, sub_group):
+                    lessons_list.append(lesson)
+                
+        return lessons_list   
+    
     def getDay(self, user_id, date: datetime):
         """
         Get all lessons for one day for given group and date
@@ -185,24 +186,9 @@ class DataBase():
         # Get all lessons for the group
         group_lessons = self.lessons.find_one({"group_id": group_id}).get("lessons")
         
-        # Initialize the list of lessons for the day
-        day_lessons = []
+        # Return the lessons for the given day
+        return self.getLessonsInDateRange(group_lessons, date, date, sub_group)
         
-        # Iterate over all lessons and select only those that are on the given day
-        for lesson in group_lessons:
-            # Check if the lesson is before the start of the day
-            if (date - lesson.get("date")).total_seconds() > 0:
-                pass
-            # Check if the lesson is after the end of the day
-            elif (lesson.get("date") - date).total_seconds() > 86300:
-                break
-            # If the lesson is on the given day, add it to the list
-            else:
-                if utils.isSubGroupValid(lesson, sub_group):
-                    day_lessons.append(lesson)
-                
-        return day_lessons   
-
     def getWeek(self, user_id, date: datetime):
         """
         Get all lessons for a week for given group and date
@@ -215,8 +201,8 @@ class DataBase():
             List[dict]: List of lessons in JSON format
         """
         start, end = utils.getStartAndEndOfWeek(date)
-            
-        user = self.users.find_one({"id": user_id})
+        
+        user = self.getUser(user_id)
         group_id = user.get("group_id")
         sub_group = user.get("sub_group")
             
@@ -224,27 +210,11 @@ class DataBase():
         if not self.isWeekInDB(group_id, date):
             raise ValueError
             
-        i = 0
-        week_lessons = []
-            
         # Get all lessons for the group
-        group_lessons = self.lessons.find_one({"group_id": group_id})
+        group_lessons = self.getLessonsForGroup(group_id)
         last_update = group_lessons.get("last_update").strftime("%d.%m %H:%M:%S")
-            
-        # Iterate over all lessons and select only those that are in the given week
-        for lesson in group_lessons.get("lessons"):
-            # Check if the lesson is before the start of the week
-            if (start - lesson.get("date")).total_seconds() > 0:
-                pass
-            # Check if the lesson is after the end of the week
-            elif (lesson.get("date") - end).total_seconds() > 86300:
-                break
-            # If the lesson is in the given week, add it to the list
-            else:
-                if utils.isSubGroupValid(lesson, sub_group):
-                    week_lessons.append(lesson)
-                
-        return week_lessons, last_update
+        
+        return self.getLessonsInDateRange(group_lessons.get("lessons"), start, end, sub_group), last_update
 
     def getGroupsList(self) -> List[str]:
         """
