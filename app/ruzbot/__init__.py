f943204a4c6456b0b0aa5b624482d2a542cc764e
Artem <wieredsh@gmail.com>
2026-04-10 19:57:15 +0300

    chore: clear deprecated main branch

--- app/ruzbot/__init__.py
+++
@@ -1,4 +0,0 @@
-from .ruzbot import bot
-from .handlers import *
-from .commands import *
-from .markups import *
--- app/utils/__init__.py
+++
@@ -1,3 +0,0 @@
-from .utils import *
-from .formatters import *
-from .daters import *
